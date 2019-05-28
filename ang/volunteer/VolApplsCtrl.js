0c7366b24f562e4ec4385a7e39f582c89d8214ef
Priyanka Shinde <priyanka.shinde@clariontechnologies.co.in>
2019-05-28 18:10:57 +0530

    Added modal pop up on click of advanced filter

--- ang/volunteer/VolApplsCtrl.js
+++ ang/volunteer/VolApplsCtrl.js
@@ -117,6 +117,65 @@
 
      }
 
+    $scope.openModal=function(){
+      $scope.modalClass="modalDialog"
+     }
+
+    $scope.closeModal=function() {
+      $scope.modalClass=null;
+    }
+
+    $scope.active = 1;
+  $scope.selectTab = function(value){
+    $scope.active = value;
+  }
+
+  $scope.isActive = function(value){
+    if($scope.active==value){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  /*$scope.openCity=function(event, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = angular.element(document.querySelector(".tabcontent"));//document.getElementsByClassName("tabcontent"); 
+  console.log(tabcontent);
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = angular.element(document.querySelector(".tablinks"));//document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  $(event.target).addClass('active');
+  //evt.currentTarget.className += " active";
+}
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();*/
+
   });
 
 })(angular, CRM.$, CRM._);
+
+
+// function openCity(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = angular.element(document.querySelector(".tabcontent"));//document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = angular.element(document.querySelector(".tablinks"));//document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
